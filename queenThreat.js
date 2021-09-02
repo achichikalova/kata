23fb1d2880c5651c8f08ecc0d8a7acfbcf9117ff
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2021-09-02 00:58:06 +0000

    Started Queen Threat Detector Assignment

---
+++ queenThreat.js
@@ -0,0 +1,14 @@
+const generateBoard = function () {
+  let boardSize = 8;
+  let chessBoard = [0,0,0,0,0,0];
+  let boardRows = [];
+  
+  // for (let i = 0; i < boardSize; i++) {
+  //   boardRows.push(0);
+  //   chessBoard.push(boardRows);    
+  // }
+    
+  return chessBoard;
+}
+
+console.log(generateBoard());
